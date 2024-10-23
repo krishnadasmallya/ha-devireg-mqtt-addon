6eec77df747e8f5ff37e745e43cc477ebc954e33
bob-tm <47008924+bob-tm@users.noreply.github.com>
2024-10-23 15:04:59 +0300

    New auto-discovery-templates logic
    User can overwrite auto-discovery-templates by putting same folder to config folder

--- devismart-mqtt/entrypoint.sh
+++ devismart-mqtt/entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-if [ ! -d "/config/auto-discovery-templates" ]; then
-    echo "Copy Config"
-    cp -av "/app/auto-discovery-templates" "/config/auto-discovery-templates"
+if [ -d "/config/auto-discovery-templates" ]; then
+    echo "Custom templates"
+    config_templates="/config/auto-discovery-templates"
+else
+    echo "Built in templates"
+    config_templates="/app/auto-discovery-templates"
 fi
 
-exec java -cp ha-devi-mqtt.jar io.homeassistant.devi.mqtt.service.ConsoleRunner --auto-discovery-templates "/config/auto-discovery-templates" --mqtt-config "/config/mqtt_config.json" --devi-config "/config/devi_config.json"
+
+exec java -cp ha-devi-mqtt.jar io.homeassistant.devi.mqtt.service.ConsoleRunner --auto-discovery-templates "$config_templates" --mqtt-config "/config/mqtt_config.json" --devi-config "/config/devi_config.json"
